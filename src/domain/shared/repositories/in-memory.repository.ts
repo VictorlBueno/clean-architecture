68a4e9588d7cd8309ff87b0dfbedc14e626599f7
victorlbueno <vihbuenoz@gmail.com>
2025-01-18 19:12:01 -0300

    Adding repository InMemoryRepository - part 2

--- src/domain/shared/repositories/in-memory.repository.ts
+++ src/domain/shared/repositories/in-memory.repository.ts
@@ -1,11 +1,15 @@
 import {RepositoryInterface} from "@/domain/shared/repositories/repository-contracts";
 import {Entity} from "@/domain/shared/entities/entity";
+import {NotFoundError} from "@/domain/shared/errors/not-found-error";
 
 export abstract class InMemoryRepository<E extends Entity> implements RepositoryInterface<E> {
     items: E[] = [];
 
     async delete(id: string): Promise<void> {
-        return Promise.resolve(undefined);
+        await this._get(id);
+
+        const index = this.items.findIndex(item => item._id === id);
+        this.items.slice(index, 1);
     }
 
     async findAll(): Promise<E[]> {
@@ -13,21 +17,28 @@ export abstract class InMemoryRepository<E extends Entity> implements Repository
     }
 
     async findById(id: string): Promise<E> {
+        return this._get(id);
+    }
+
+    async insert(entity: E): Promise<void> {
+        this.items.push(entity);
+    }
+
+    async update(entity: E): Promise<void> {
+        await this._get(entity._id);
+
+        const index = this.items.findIndex(item => item._id === entity._id);
+        this.items[index] = entity;
+    }
+
+    protected async _get(id: string): Promise<E> {
         const _id = `${id}`;
         const entity = this.items.find((item) => item._id === _id);
 
         if (!entity) {
-            throw new Error("Entity not found.");
+            throw new NotFoundError("Entity not found.");
         }
 
         return entity;
-    }
-
-    async insert(E): Promise<void> {
-        this.items.push(new E());
-    }
-
-    async update(E): Promise<void> {
-        return Promise.resolve(undefined);
     }
 }
