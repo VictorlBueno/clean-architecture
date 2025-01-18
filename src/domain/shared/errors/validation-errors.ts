a612f81d17f7b4ccf876648151919d5a333f9f80
victorlbueno <vihbuenoz@gmail.com>
2025-01-18 18:29:42 -0300

    Integration tests - validate name field on create user

--- src/domain/shared/errors/validation-errors.ts
+++ src/domain/shared/errors/validation-errors.ts
@@ -3,7 +3,7 @@ import {FieldsError} from "@/domain/shared/validators/validator-fields.interface
 export class ValidationError extends Error {
 }
 
-export class EntityValidationError extends Error {
+export class EntityValidationError extends ValidationError {
     constructor(public error: FieldsError) {
         super("Entity validation error");
         this.name = "EntityValidationError";
