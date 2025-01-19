31705cdd3c4953191a2324d1f3177352f7be7cd5
victorlbueno <vihbuenoz@gmail.com>
2025-01-19 18:13:38 -0300

    Adding forTest static method

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import {AppService} from "./app.service";
 import {EnvConfigModule} from "./infrastructure/modules/env-config.module";
 import { UsersModule } from './infrastructure/modules/users.module';
 import {DatabaseModule} from "@/infrastructure/modules/database.module";
-import { PrismaService } from './infrastructure/shared/database/prisma/prisma/prisma.service';
+import {PrismaService} from "@/infrastructure/shared/database/prisma/prisma.service";
 
 @Module({
     imports: [EnvConfigModule, UsersModule, DatabaseModule],
--- src/infrastructure/modules/database.module.ts
+++ src/infrastructure/modules/database.module.ts
@@ -1,8 +1,10 @@
-import {Module} from "@nestjs/common";
+import {DynamicModule, Global, Module} from "@nestjs/common";
 import {EnvConfigModule} from "@/infrastructure/modules/env-config.module";
 import {ConfigService} from "@nestjs/config";
-import {PrismaService} from "@/infrastructure/shared/database/prisma/prisma/prisma.service";
+import {PrismaClient} from "@prisma/client";
+import {PrismaService} from "@/infrastructure/shared/database/prisma/prisma.service";
 
+@Global()
 @Module({
     imports: [EnvConfigModule.forRoot()],
     providers: [ConfigService],
@@ -10,4 +12,15 @@ import {PrismaService} from "@/infrastructure/shared/database/prisma/prisma/pris
 })
 
 export class DatabaseModule {
+    static forTest(prismaClient: PrismaClient): DynamicModule {
+        return {
+            module: DatabaseModule,
+            providers: [
+                {
+                    provide: PrismaService,
+                    useFactory: () => prismaClient as PrismaService,
+                },
+            ],
+        };
+    }
 }
